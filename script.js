d7e9b4ebb2de471787e2f30bddcd74204c6d6668
test-development-cloud <81577608+test-development-cloud@users.noreply.github.com>
2021-03-29 11:14:38 -0500

    se quita tolowercase
    se quita tolowercase en el js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btnSearch = document.getElementById("btnSearch");
 
 inputSearch.addEventListener("change", ()=>{
     const valueSerarch = inputSearch.value;
-    const inputSearchClean = valueSerarch.replace(/[^-A-Za-z0-9]+/g, '_').toLowerCase();
+    const inputSearchClean = valueSerarch.replace(/[^-A-Za-z0-9]+/g, '_');
     const link = inputSearchClean + ".html";
     btnSearch.href = link;
 })
